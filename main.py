8903a5bfd12cf61e81a1af167930bbc37f7e82fc
itsindraxx <rayanghailantagi@gmail.com>
2026-01-15 20:42:16 +0100

    Archivos iniciales para el proyecto MakeCode

---
+++ main.py
@@ -0,0 +1,25 @@
+def on_on_chat():
+    mobs.apply_effect(NIGHT_VISION, mobs.target(NEAREST_PLAYER), 1e+221, 255)
+    player.execute("say Vale bro")
+player.on_chat("iaiaayeyeyeye", on_on_chat)
+
+pixelArt.draw_image(img("""
+        5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5
+        5 f f 5 5 5 5 5 5 5 5 5 5 f f 5
+        5 f 1 5 5 5 5 5 5 5 5 5 5 1 f 5
+        5 5 5 5 5 5 5 f 5 5 5 5 5 5 5 5
+        5 5 5 5 5 5 5 f 5 5 5 5 5 5 5 5
+        5 5 5 5 5 5 5 5 f 5 5 5 5 5 5 5
+        5 5 5 5 5 5 5 f 5 5 5 5 5 5 5 5
+        5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5
+        5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5
+        5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5
+        5 f 5 5 5 5 5 5 5 5 5 5 5 5 f 5
+        5 5 f 5 5 5 5 5 5 5 5 5 5 f 5 5
+        5 5 5 f 5 5 5 5 5 5 5 5 f 5 5 5
+        5 5 5 5 f f f f f f f f 5 5 5 5
+        5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5
+        5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5
+        """),
+    pos(0, 100, 0),
+    WEST)
---
+++ main.ts
@@ -0,0 +1,25 @@
+player.onChat("iaiaayeyeyeye", function () {
+    mobs.applyEffect(NIGHT_VISION, mobs.target(NEAREST_PLAYER), 1e+221, 255)
+    castles.buildSimpleCastle(COBBLESTONE)
+    player.execute(
+    "say Vale bro"
+    )
+})
+pixelArt.drawImage(img`
+    5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 
+    5 f f 5 5 5 5 5 5 5 5 5 5 f f 5 
+    5 f 1 5 5 5 5 5 5 5 5 5 5 1 f 5 
+    5 5 5 5 5 5 5 f 5 5 5 5 5 5 5 5 
+    5 5 5 5 5 5 5 f 5 5 5 5 5 5 5 5 
+    5 5 5 5 5 5 5 5 f 5 5 5 5 5 5 5 
+    5 5 5 5 5 5 5 f 5 5 5 5 5 5 5 5 
+    5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 
+    5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 
+    5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 
+    5 f 5 5 5 5 5 5 5 5 5 5 5 5 f 5 
+    5 5 f 5 5 5 5 5 5 5 5 5 5 f 5 5 
+    5 5 5 f 5 5 5 5 5 5 5 5 f 5 5 5 
+    5 5 5 5 f f f f f f f f 5 5 5 5 
+    5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 
+    5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 
+    `, pos(0, 100, 0), WEST)
