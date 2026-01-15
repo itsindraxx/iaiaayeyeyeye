19ffd5378867c4ebc4265f5e299b2f1a8f42ea54
itsindraxx <rayanghailantagi@gmail.com>
2026-01-15 20:43:45 +0100

    Actualizar main.blocks, main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 def on_on_chat():
     mobs.apply_effect(NIGHT_VISION, mobs.target(NEAREST_PLAYER), 1e+221, 255)
+    castles.build_simple_castle(COBBLESTONE)
     player.execute("say Vale bro")
 player.on_chat("iaiaayeyeyeye", on_on_chat)
 
